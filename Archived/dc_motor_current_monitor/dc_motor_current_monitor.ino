f7867480340fdf25c95e4b846bf7a3bfdae5d202
yyeung <yyeung@mit.edu>
2022-03-21 15:33:04 -0400

    current dc open loop code

---
+++ Archived/dc_motor_current_monitor/dc_motor_current_monitor.ino
@@ -0,0 +1,49 @@
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+
+
+Adafruit_INA219 ina219; //Init current sensor, A4 to SDA, A5 to SCL
+
+int motor1pin1 = 2; //Init motor pins
+int motor1pin2 = 3;
+int motor1ena = 9;
+float current_mA = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Setup");
+
+  uint32_t currentFrequency;
+  while (!Serial) {
+      // will pause Zero, Leonardo, etc until serial console opens
+      delay(1);
+  }
+
+  if (! ina219.begin()) {
+    Serial.println("Failed to find INA219 chip");
+    while (1) { delay(10); }
+  }
+  
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor1ena, OUTPUT); 
+
+}
+
+void loop() {
+  //Controlling speed (0 = off and 255 = max speed):
+  analogWrite(motor1ena, 255); //ENA pin
+
+  //Controlling spin direction of motors
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+
+  current_mA = ina219.getCurrent_mA();
+  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+  delay(100);
+
+  
+//  digitalWrite(motor1pin1, LOW);
+//  digitalWrite(motor1pin2, HIGH);
+}
--- Archived/sketch_mar08a/sketch_mar08a.ino
+++ Archived/sketch_mar08a/sketch_mar08a.ino
@@ -5,21 +5,19 @@ int motor2pin1 = 4;
 int motor2pin2 = 5;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Setup");
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
-//  pinMode(motor2pin1, OUTPUT);
-//  pinMode(motor2pin2, OUTPUT);
+
 
   pinMode(9, OUTPUT); 
+//  pinMode(motor2pin1, OUTPUT);
+//  pinMode(motor2pin2, OUTPUT);
 //  pinMode(10, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:   
-
   //Controlling speed (0 = off and 255 = max speed):
   analogWrite(9, 255); //ENA pin
 //  analogWrite(10, 200); //ENB pin
@@ -33,8 +31,8 @@ void loop() {
 //  digitalWrite(motor2pin2, LOW);
   delay(100);
 
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, HIGH);
+//  digitalWrite(motor1pin1, LOW);
+//  digitalWrite(motor1pin2, HIGH);
 
 //  digitalWrite(motor2pin1, LOW);
 //  digitalWrite(motor2pin2, HIGH);
