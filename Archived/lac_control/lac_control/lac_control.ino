46cf9379ead857aa759dd6c5540847e3cfcdaf30
yyeung <yyeung@mit.edu>
2022-03-22 19:27:11 -0400

    LAC control with potentiometer reading

---
+++ Archived/lac_control/lac_control/lac_control.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(13);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    Serial.println(analogRead(A5)); // potentiometer reading
+
+    delay(30);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    Serial.println(analogRead(A5));
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(30);                       // waits 15ms for the servo to reach the position
+  }
+}
